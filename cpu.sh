2e7ccdd7fe48d7a3cb2bdc5b0643c7e7b566f6e3
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-02-28 19:26:31 -0500

    thread control

--- cpu.sh
+++ cpu.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Advanced CPU throttling
+clear
+cat << "EOF"
+____   _______  _______    ____   _______  _______    ____   _______  _______ 
+|   | |  _    ||       |  |    | |       ||  _    |  |    | |       ||       |
+|   | | | |   ||___    |   |   | |____   || | |   |   |   | |____   ||   ____|
+|   | | | |   | ___|   |   |   |  ____|  || | |   |   |   |  ____|  ||  |____ 
+|   | | |_|   ||___    |   |   | | ______|| |_|   |   |   | | ______||_____  |
+|   | |       | ___|   |   |   | | |_____ |       |   |   | | |_____  _____| |
+|___| |_______||_______|   |___| |_______||_______|   |___| |_______||_______|
+POC
+EOF
+
+#How many cores?
+echo "How many threads can I use?"
+read thread
+clear
+echo "Press Ctrl+C $thread times to stop each job"
+
+# Advanced CPU throttling loop
 set -m #Enables Job ctrl
-for i in `seq 1000`; do #1000 threads
-    seq 16 | xargs -P0 -n1 md5sum /dev/zero # Max out up 16 Cores
+for i in `seq $thread`; do
+    seq $thread | xargs -P0 -n1 md5sum /dev/zero
 done
